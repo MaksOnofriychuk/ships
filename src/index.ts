5a360631a8200c20624f54157676fbd1f4586f75
MaksOnofriychuk <maksonofrijcuk002@gmail.com>
2024-04-20 11:45:26 +0300

    create project

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+import "pixi-spine";
+import "./style.css";
+import { Application } from "pixi.js";
+
+const gameWidth = 1280;
+const gameHeight = 720;
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const app = new Application<HTMLCanvasElement>({
+    backgroundColor: 0xd3d3d3,
+    width: gameWidth,
+    height: gameHeight,
+});
